164fe12aa227c6b6c8a4f5ff32fe3ac551b52497
Emerson Jacobson <emwjacobson@gmail.com>
2020-11-18 20:55:09 -0800

    Added Base Encounter class

---
+++ header/factory/encounter.hpp
@@ -0,0 +1,15 @@
+#ifndef ENCOUNTER_HPP
+#define ENCOUNTER_HPP
+
+#include <string>
+
+class Encounter
+{
+private:
+    std::string encounter_name;
+public:
+    Encounter(std::string name) : encounter_name(name) {};
+    virtual std::string getEncounter() = 0;
+};
+
+#endif // ENCOUNTER_HPP
