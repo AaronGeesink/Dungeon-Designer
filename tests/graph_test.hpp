7d428e991fe8b5ff5733c882dc1844d0315deb95
roveredpwn <roverdog56@gmail.com>
2020-11-21 22:02:44 -0800

    Added new graph class functions and unit tests - Vastly improved the functionality of the matrix graph class 	- Allowed for the graph class to preallocate memory without having to use it to store vertices 	- There are now two more constructors for adding an empty graph or a graph with preallocated memory 	- Made the addVertex function more data efficient, so that it expands only if the data is necessary 	- Added a getSize() function to get the number of vertices 	- Added a getMaxSize() function to get the total number of vertices the graph can store before needing to expand 	- Added a clear() function to empty the graph of its vertices and preallocated memory 	- Fixed a ton of edge cases so that they are no longer ambiguous - Added unit tests in graph_test.cpp to test the new functions as well as a lot of edge cases - Overall, the graph class is more functional and data efficient while having less undefined behavior

--- tests/graph_test.hpp
+++ tests/graph_test.hpp
@@ -6,9 +6,72 @@
 
 #include "../header/matrix_graph.hpp"
 
-TEST(GraphTest, InitializationTest) {
+TEST(GraphTest, NoParameterConstructorTest) {
+    MatrixGraph<int> graph;
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id \n";
+
+    EXPECT_EQ(graph.displayGraph(), test);
+    EXPECT_EQ(graph.getSize(), 0);
+    EXPECT_EQ(graph.getMaxSize(), 0);
+
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+
+    test = "Here is the adjacency matrix:\n"
+        " id  0   1   2   3   4  \n"
+        "  0  0   0   0   0   0  \n"
+        "  1  0   0   0   0   0  \n"
+        "  2  0   0   0   0   0  \n"
+        "  3  0   0   0   0   0  \n"
+        "  4  0   0   0   0   0  \n";
+
+    EXPECT_EQ(graph.displayGraph(), test);
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.getMaxSize(), 6);
+}
+
+TEST(GraphTest, SingleParameterConstructorTest) {
+    MatrixGraph<int> graph(5);
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id \n";
+
+    EXPECT_EQ(graph.displayGraph(), test);
+    EXPECT_EQ(graph.getSize(), 0);
+    EXPECT_EQ(graph.getMaxSize(), 5);
+
+    graph.addVertex(0);
+    graph.addVertex(1);
+    graph.addVertex(2);
+    graph.addVertex(3);
+    graph.addVertex(4);
+
+    test = "Here is the adjacency matrix:\n"
+        " id  0   1   2   3   4  \n"
+        "  0  0   0   0   0   0  \n"
+        "  1  0   0   0   0   0  \n"
+        "  2  0   0   0   0   0  \n"
+        "  3  0   0   0   0   0  \n"
+        "  4  0   0   0   0   0  \n";
+
+    EXPECT_EQ(graph.displayGraph(), test);
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.getMaxSize(), 5);
+}
+
+TEST(GraphTest, NegativeSingleParameterConstructorTest) {
+
+    ASSERT_ANY_THROW(MatrixGraph<int> graph(-1));
+}
+
+TEST(GraphTest, SameSizeInitializationTest) {
     int numbers[5] = { 1,2,3,4,5 };
-    MatrixGraph<int> graph(numbers, 5);
+    MatrixGraph<int> graph(numbers, 5, 5);
 
     std::string test = "Here is the adjacency matrix:\n"
         " id  0   1   2   3   4  \n"
@@ -18,22 +81,79 @@ TEST(GraphTest, InitializationTest) {
         "  3  0   0   0   0   0  \n"
         "  4  0   0   0   0   0  \n";
 
+    EXPECT_EQ(graph.getSize(), 5);
     EXPECT_EQ(graph.displayGraph(), test);
 }
 
-TEST(GraphTest, ZeroInitializationTest) {
+TEST(GraphTest, NegativeArrayInitializationTest) {
     int numbers[5] = { 1,2,3,4,5 };
-    MatrixGraph<int> graph(numbers, 0);
+    MatrixGraph<int> graph(numbers, -1, 5);
 
     std::string test = "Here is the adjacency matrix:\n"
         " id \n";
 
+    EXPECT_EQ(graph.getSize(), 0);
+    EXPECT_EQ(graph.getMaxSize(), 5);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, NegativeGraphInitializationTest) {
+    int numbers[5] = { 1,2,3,4,5 };
+    ASSERT_ANY_THROW(MatrixGraph<int> graph(numbers, 5, -1));
+}
+
+TEST(GraphTest, BothNegativeInitializationTest) {
+    int numbers[5] = { 1,2,3,4,5 };
+    ASSERT_ANY_THROW(MatrixGraph<int> graph(numbers, -1, -1));
+}
+
+TEST(GraphTest, BiggerGraphThanArrayInitializationTest) {
+    int numbers[5] = { 1,2,3,4,5 };
+    MatrixGraph<int> graph(numbers, 5, 7);
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id  0   1   2   3   4  \n"
+        "  0  0   0   0   0   0  \n"
+        "  1  0   0   0   0   0  \n"
+        "  2  0   0   0   0   0  \n"
+        "  3  0   0   0   0   0  \n"
+        "  4  0   0   0   0   0  \n";
+
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, SmallerGraphThanArrayInitializationTest) {
+    int numbers[7] = { 1,2,3,4,5,6,7 };
+    // graph will only insert the first 5 ints from the array.
+    MatrixGraph<int> graph(numbers, 7, 5);
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id  0   1   2   3   4  \n"
+        "  0  0   0   0   0   0  \n"
+        "  1  0   0   0   0   0  \n"
+        "  2  0   0   0   0   0  \n"
+        "  3  0   0   0   0   0  \n"
+        "  4  0   0   0   0   0  \n";
+
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, ZeroGraphSizeInitializationTest) {
+    int numbers[5] = { 1,2,3,4,5 };
+    MatrixGraph<int> graph(numbers, 5, 0);
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id \n";
+
+    EXPECT_EQ(graph.getSize(), 0);
     EXPECT_EQ(graph.displayGraph(), test);
 }
 
 TEST(GraphTest, GetNodeTest) {
     int numbers[5] = { 0,1,2,3,4 };
-    MatrixGraph<int> graph(numbers, 5);
+    MatrixGraph<int> graph(numbers, 5, 5);
 
     EXPECT_EQ(graph.getNode(0), 0);
     EXPECT_EQ(graph.getNode(1), 1);
@@ -44,7 +164,7 @@ TEST(GraphTest, GetNodeTest) {
 
 TEST(GraphTest, BidirectionalEdgeTest) {
     int numbers[5] = { 0,1,2,3,4 };
-    MatrixGraph<int> graph(numbers, 5);
+    MatrixGraph<int> graph(numbers, 5, 5);
 
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
@@ -59,12 +179,13 @@ TEST(GraphTest, BidirectionalEdgeTest) {
         "  3  0   0   1   0   1  \n"
         "  4  0   0   0   1   0  \n";
 
+    EXPECT_EQ(graph.getSize(), 5);
     EXPECT_EQ(graph.displayGraph(), test);
 }
 
 TEST(GraphTest, IdenticalNodesTest) {
     int numbers[5] = { 0,0,0,0,0 };
-    MatrixGraph<int> graph(numbers, 5);
+    MatrixGraph<int> graph(numbers, 5, 5);
 
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
@@ -79,19 +200,22 @@ TEST(GraphTest, IdenticalNodesTest) {
         "  3  0   0   1   0   1  \n"
         "  4  0   0   0   1   0  \n";
 
+    EXPECT_EQ(graph.getSize(), 5);
     EXPECT_EQ(graph.displayGraph(), test);
 }
 
-TEST(GraphTest, AddVertexTest) {
+TEST(GraphTest, AddVertexNoExpandTest) {
     int numbers[4] = { 0,1,2,3 };
-    MatrixGraph<int> graph(numbers, 4);
+    MatrixGraph<int> graph(numbers, 4, 5);
 
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
     graph.addEdge(2, 3);
 
-    graph.addVertex(4);
+    EXPECT_EQ(graph.getSize(), 4);
+    EXPECT_EQ(graph.getMaxSize(), 5);
 
+    graph.addVertex(4);
     graph.addEdge(3, 4);
 
     std::string test = "Here is the adjacency matrix:\n"
@@ -102,16 +226,84 @@ TEST(GraphTest, AddVertexTest) {
         "  3  0   0   1   0   1  \n"
         "  4  0   0   0   1   0  \n";
 
-    EXPECT_EQ(graph.displayGraph(), test);
-
-    EXPECT_EQ(graph.size(), 5);
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.getMaxSize(), 5);
     EXPECT_EQ(graph.getNode(4), 4);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, AddVertexWithExpandTest) {
+    int numbers[4] = { 0,1,2,3 };
+    MatrixGraph<int> graph(numbers, 4, 4);
+
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+
+    EXPECT_EQ(graph.getSize(), 4);
+    EXPECT_EQ(graph.getMaxSize(), 4);
+
+    graph.addVertex(4);
+    graph.addEdge(3, 4);
+
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id  0   1   2   3   4  \n"
+        "  0  0   1   0   0   0  \n"
+        "  1  1   0   1   0   0  \n"
+        "  2  0   1   0   1   0  \n"
+        "  3  0   0   1   0   1  \n"
+        "  4  0   0   0   1   0  \n";
+
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.getMaxSize(), 6); // 4 * 1.5 = 6
+    EXPECT_EQ(graph.getNode(4), 4);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, AddVertexExpandFromZeroTest) {
+    int numbers[4] = { 0,1,2,3 };
+    MatrixGraph<int> graph(numbers, 4, 0);
+
+    EXPECT_EQ(graph.getSize(), 0);
+    EXPECT_EQ(graph.getMaxSize(), 0);
+
+    graph.addVertex(10);
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id  0  \n"
+        "  0  0  \n";
+
+    EXPECT_EQ(graph.getSize(), 1);
+    EXPECT_EQ(graph.getMaxSize(), 2);
+    EXPECT_EQ(graph.getNode(0), 10);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, AddVertexExpandFromOneTest) {
+    int numbers[4] = { 0,1,2,3 };
+    MatrixGraph<int> graph(numbers, 4, 1);
+
+    EXPECT_EQ(graph.getSize(), 1);
+    EXPECT_EQ(graph.getMaxSize(), 1);
+
+    graph.addVertex(10);
+
+    std::string test = "Here is the adjacency matrix:\n"
+        " id  0   1  \n"
+        "  0  0   0  \n"
+        "  1  0   0  \n";
+
+    EXPECT_EQ(graph.getSize(), 2);
+    EXPECT_EQ(graph.getMaxSize(), 2);
+    EXPECT_EQ(graph.getNode(0), 0);
+    EXPECT_EQ(graph.getNode(1), 10);
     EXPECT_EQ(graph.displayGraph(), test);
 }
 
 TEST(GraphTest, ClearGraphTest) {
     int numbers[4] = { 0,1,2,3 };
-    MatrixGraph<int> graph(numbers, 4);
+    MatrixGraph<int> graph(numbers, 4, 4);
 
     graph.addEdge(0, 1);
     graph.addEdge(1, 2);
@@ -119,13 +311,52 @@ TEST(GraphTest, ClearGraphTest) {
 
     graph.clear();
 
+    std::string test = "Here is the adjacency matrix:\n"
+        " id \n";
+
+    EXPECT_EQ(graph.displayGraph(), test);
+
     graph.addVertex(4);
     graph.addVertex(5);
     graph.addVertex(6);
     graph.addVertex(7);
     graph.addVertex(8);
 
+    graph.addEdge(0, 1);
+    graph.addEdge(1, 2);
+    graph.addEdge(2, 3);
+    graph.addEdge(3, 4);
+
+    test = "Here is the adjacency matrix:\n"
+        " id  0   1   2   3   4  \n"
+        "  0  0   1   0   0   0  \n"
+        "  1  1   0   1   0   0  \n"
+        "  2  0   1   0   1   0  \n"
+        "  3  0   0   1   0   1  \n"
+        "  4  0   0   0   1   0  \n";
+
+    EXPECT_EQ(graph.getSize(), 5);
+    EXPECT_EQ(graph.getNode(0), 4);
+    EXPECT_EQ(graph.getNode(4), 8);
+    EXPECT_EQ(graph.displayGraph(), test);
+}
+
+TEST(GraphTest, ClearEmptyGraphTest) {
+    MatrixGraph<int> graph;
+    graph.clear();
+
     std::string test = "Here is the adjacency matrix:\n"
+        " id \n";
+
+    EXPECT_EQ(graph.displayGraph(), test);
+
+    graph.addVertex(4);
+    graph.addVertex(5);
+    graph.addVertex(6);
+    graph.addVertex(7);
+    graph.addVertex(8);
+
+    test = "Here is the adjacency matrix:\n"
         " id  0   1   2   3   4  \n"
         "  0  0   0   0   0   0  \n"
         "  1  0   0   0   0   0  \n"
@@ -133,9 +364,7 @@ TEST(GraphTest, ClearGraphTest) {
         "  3  0   0   0   0   0  \n"
         "  4  0   0   0   0   0  \n";
 
-    EXPECT_EQ(graph.displayGraph(), test);
-
-    EXPECT_EQ(graph.size(), 5);
+    EXPECT_EQ(graph.getSize(), 5);
     EXPECT_EQ(graph.getNode(0), 4);
     EXPECT_EQ(graph.getNode(4), 8);
     EXPECT_EQ(graph.displayGraph(), test);
